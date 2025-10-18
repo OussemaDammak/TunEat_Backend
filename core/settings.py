9b61e761c83aa66181289193f2829ffa3a743124
Oussema Dammak <oussemadammak01@gmail.com>
2025-10-18 19:24:52 +0100

    prepare for deployment

--- core/settings.py
+++ core/settings.py
@@ -169,7 +169,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS= [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://*.onrender.com',
+    "https://*.onrender.com",
+    "https://tuneat.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS= True
